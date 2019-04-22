5828465c2690c0c1eae8b0c2ca593aa933f0df55
Ignacio Paradisi <ignacioparadisi@gmail.com>
2019-04-22 19:42:55 -0400

    feat: Agregar script para crear la base de datos
    Se agregó el script para crear el usuario y la base de datos en PostgreSQL

---
+++ BD_Scripts/create_db.sql
@@ -0,0 +1,19 @@
+DROP USER IF EXISTS "vacanza";
+
+CREATE USER "vacanza" WITH
+    LOGIN
+    SUPERUSER
+    INHERIT
+    CREATEDB
+    CREATEROLE
+    ENCRYPTED PASSWORD 'vacanza'
+    NOREPLICATION;
+
+CREATE DATABASE "vacanza"
+    WITH
+    OWNER = "vacanza"
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
