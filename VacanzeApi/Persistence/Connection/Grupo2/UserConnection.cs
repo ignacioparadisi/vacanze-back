a299de9f75e43f61f4c8162e4a6c298583fcb2d6
Ignacio Paradisi <ignacioparadisi@gmail.com>
2019-05-26 10:25:05 -0400

    Feat: Se regresan los empleados al hacer request por ellos

---
+++ VacanzeApi/Persistence/Connection/Grupo2/UserConnection.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Npgsql;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo2;
+using vacanze_back.VacanzeApi.Common.Exceptions;
+
+namespace vacanze_back.VacanzeApi.Persistence.Connection.Grupo2
+{
+    public class UserConnection : Connection
+    {
+        public UserConnection()
+        {
+            CreateStringConnection();
+        }
+        
+        /// <summary>
+        /// Método para obtener todos los empleados de la BD
+        /// </summary>
+        /// <returns>Lista de usuarios que son empleados</returns>
+        public List<User> GetEmployees()
+        {
+            var users = new List<User>();
+            try
+            {
+                Connect();
+                StoredProcedure("getEmployees()");
+                ExecuteReader();
+                for (int i = 0; i < numberRecords; i++)
+                {
+                    var id = Convert.ToInt64(GetString(i, 0));
+                    var documentId = Convert.ToInt64(GetString(i, 1));
+                    var name = GetString(i, 2);
+                    var lastname = GetString(i, 3);
+                    var email = GetString(i, 4);
+                    var user = new User(id, documentId, name, lastname, email);
+                    users.Add(user);
+                }
+            }
+            catch (NpgsqlException e)
+            {
+                throw new DatabaseException("Error al buscar empleados");
+            }
+            catch (Exception e)
+            {
+                throw new GeneralException(e, DateTime.Now);
+            }
+
+            return users;
+        }
+    }
+}
