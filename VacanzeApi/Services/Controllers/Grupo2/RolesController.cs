d686a7db721ac1ece143149d06bb3a8a577c32a4
gabrielbaron16 <gabrielbu161@gmail.com>
2019-05-25 22:48:06 -0400

    Añadiendo Endpoint para Consultar Roles

--- VacanzeApi/Services/Controllers/Grupo2/RolesController.cs
+++ VacanzeApi/Services/Controllers/Grupo2/RolesController.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using vacanze_back.Common.Entities.Grupo2;
+using vacanze_back.Common.Exceptions;
+using vacanze_back.Persistence.Connection;
+using vacanze_back.Persistence.Connection.Grupo2;
 
 namespace vacanze_back.Services.Controllers.Grupo2
 {
@@ -12,15 +15,20 @@ namespace vacanze_back.Services.Controllers.Grupo2
     {   
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<Role>> Get()
+        public ActionResult<IEnumerable<Role>> GetRoles()
         {
-            List<Role> roles = new List<Role>();
-            roles.Add(new Role(0, "Cliente"));
-            roles.Add(new Role(1, "Administrador"));
-            roles.Add(new Role(2, "Checkin"));
-            roles.Add(new Role(3, "Claim"));
-            roles.Add(new Role(4, "Cargador"));
-            return Ok(roles.ToList());
+            var con = new RoleConnection();
+            var roles = new List<Role>();
+            try
+            {
+                roles = con.GetRoles();
+            }
+            catch (DatabaseException e)
+            {
+                return Ok(e.Message);
+            }
+
+            return roles;
         }
 
         // GET api/values/5
