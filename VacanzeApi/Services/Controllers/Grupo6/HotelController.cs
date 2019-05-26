7208eb874cab74d5e099b951728c5881567eba59
cesarbonadio <cesarbonadio123@gmail.com>
2019-05-26 15:56:13 -0400

    feat: añadir controlador de hoteles

---
+++ VacanzeApi/Services/Controllers/Grupo6/HotelController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo6;
+using vacanze_back.VacanzeApi.Common.Exceptions;
+using vacanze_back.VacanzeApi.Persistence.Connection.Grupo6;
+
+
+namespace vacanze_back.VacanzeApi.Services.Controllers.Grupo6
+{
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [EnableCors("MyPolicy")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+
+        // GET api/Hotel
+        [HttpGet]
+        public ActionResult<IEnumerable<Hotel>> GetHotels()
+        {
+            var con = new HotelConnection();
+            var hotels = new List<Hotel>();
+            try
+            {
+                hotels = con.GetHotels();
+            }
+            catch (DatabaseException e)
+            {
+                return Ok(e.Message);
+            }
+
+            return hotels;
+        }
+
+    }
+}
