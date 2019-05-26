f316b22cc99df8aa891cf8201bbe069673ab8dee
Ignacio Paradisi <ignacioparadisi@gmail.com>
2019-05-26 10:39:18 -0400

    Feat: Se agregan los roles a sus respectivos usuarios para ser devueltos en el request

---
+++ VacanzeApiTest/Grupo2/UsersTests.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+using vacanze_back.VacanzeApi.Services.Controllers.Grupo2;
+
+namespace vacanze_back.VacanzeApiTest.Grupo2
+{
+    [TestFixture]
+    public class UsersTests
+    {
+        [Test]
+        public void GetEmployeesTest()
+        {
+
+        }
+    }
+}
