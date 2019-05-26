3df949716d7478360929f77d3c2fef6bd1887efd
Ignacio Paradisi <ignacioparadisi@gmail.com>
2019-05-26 15:39:58 -0400

    Feat: Se hicieron las pruebas de Encryptor

---
+++ VacanzeApiTest/Grupo2/EncryptorTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using vacanze_back.VacanzeApi.Common;
+
+namespace vacanze_back.VacanzeApiTest.Grupo2
+{
+    [TestFixture]
+    public class EncryptorTests
+    {
+        private const string ExpectedResult = "25d55ad283aa400af464c76d713c07ad";
+        private const string TextToBeEncrypted = "12345678";
+
+        [Test]
+        public void EncryptTest()
+        {
+            var result = Encryptor.Encrypt(TextToBeEncrypted);
+            Assert.AreEqual(ExpectedResult, result);
+        }
+
+        [Test]
+        public void VerifyTest()
+        {
+            Assert.True(Encryptor.Verify(TextToBeEncrypted, ExpectedResult));
+        }
+    }
+}
