0327680f9f8d54574d5f9d00075e4cf5fae1b2ee
Juan Goncalves <juan.goncalves498@gmail.com>
2019-05-26 18:03:13 -0400

    refactor: renombrado controlador de hoteles de singular a plural
    Agregado manejo de parametro para filtro de hoteles por ubicacion

--- VacanzeApi/Services/Controllers/Grupo6/HotelsController.cs
+++ VacanzeApi/Services/Controllers/Grupo6/HotelsController.cs
@@ -1,39 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo6;
 using vacanze_back.VacanzeApi.Common.Exceptions;
 using vacanze_back.VacanzeApi.Persistence.Connection.Grupo6;
 
-
 namespace vacanze_back.VacanzeApi.Services.Controllers.Grupo6
 {
-
     [Produces("application/json")]
     [Route("api/[controller]")]
     [EnableCors("MyPolicy")]
     [ApiController]
-    public class HotelController : ControllerBase
+    public class HotelsController : ControllerBase
     {
-
-        // GET api/Hotel
+        // GET api/hotels/[?location={location_id}]
         [HttpGet]
-        public ActionResult<IEnumerable<Hotel>> GetHotels()
+        public ActionResult<IEnumerable<Hotel>> GetHotels([FromQuery] long location = -1)
         {
-            var con = new HotelConnection();
-            var hotels = new List<Hotel>();
+            var dbConnection = new HotelConnection();
             try
             {
-                hotels = con.GetHotels();
+                if (location == -1)
+                    return dbConnection.GetHotels();
+                return Ok($"No implementado todavia. Recibi location: {location}");
             }
             catch (DatabaseException e)
             {
+                // TODO: No se deberia mandar un Ok cuando falla la base de datos, se deberia mandar
+                //       un error 500 o algo
                 return Ok(e.Message);
             }
-
-            return hotels;
         }
-
     }
 }
