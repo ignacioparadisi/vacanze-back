979d953386630d498c24041e0c89ee6e1993d0bc
zilus13 <luisgermenher@gmail.com>
2019-05-28 16:59:54 -0400

    Subiendo carpetas de grupo4

--- VacanzeApi/Common/Entities/Grupo4/Baggage.cs
+++ VacanzeApi/Common/Entities/Grupo4/Baggage.cs
@@ -2,19 +2,19 @@ namespace vacanze_back.VacanzeApi.Common.Entities.Grupo4
 {
      public class Baggage : Entity
      {
-          private int MaletaId { get; set; }
-          private string MaletaFkVuelo { get; set; }
-          private string MaletaFkCrucero { get; set; }
-          private string MaletaStatus { get; set; }
-          private string CantidadPasajeros { get; set; }
-          private string Descripcion { get; set; }
+          public long MaletaId { get; set; }
+          public long MaletaFkVuelo { get; set; }
+          public long MaletaFkCrucero { get; set; }
+          public string MaletaStatus { get; set; }
+          public string CantidadPasajeros { get; set; }
+          public string Descripcion { get; set; }
 
 
 
 
           // TODO: Cuando se cree la clase Baggage, implementar esta parte
 
-          public Baggage(int id, string maletaFkVuelo, string maletaFkCrucero,
+          public Baggage(long id, long maletaFkVuelo, long maletaFkCrucero,
               string maletaStatus, string descripcion) : base(0)
           {
                MaletaId = id;
--- VacanzeApi/Services/Controllers/Grupo4/BaggageController.cs
+++ VacanzeApi/Services/Controllers/Grupo4/BaggageController.cs
@@ -8,162 +8,49 @@ using vacanze_back.VacanzeApi.Persistence.Connection.Grupo4;
 
 namespace vacanze_back.VacanzeApi.Services.Controllers.Grupo4
 {
-	[Produces("application/json")] 
-	[Route("api/[controller]")]
-	[EnableCors("MyPolicy")]
-	[ApiController]
-	public class BaggageController : ControllerBase
-	{
-        
-
-		// GET api/values
-		//se usara para consultar por pasaporte
-		[HttpGet]
-		public ActionResult<IEnumerable<Baggage>> Get()
-		{
-			try{
-                BaggageConnection conec = new ClaimConnection();
-				List<Claim> claimList= conec.GetClaim(2);
-				return Ok(claimList); 
-			}catch (DatabaseException )
-			{            
-				return StatusCode(500);
-			}
-			catch (GeneralException )
-			{
-				return StatusCode(500);
-			}
-		}
-
-		// GET api/values/5
-		[HttpGet("{id}")]
-		public ActionResult<IEnumerable<Claim>> Get(int id)
-		{
-			try{
-				ClaimConnection conec= new ClaimConnection();
-				List<Claim> claimList = conec.GetClaim(id);
-
-				return Ok(claimList); 
-			}catch (DatabaseException )
-			{            
-				return StatusCode(500);
-			}
-			catch (GeneralException )
-			{
-				return StatusCode(500);
-			}
-		}
-
-		// GET api/values/5
-		// Get para la tabla equipaje
-		[HttpGet("{tipo}/{id}")]
-		public ActionResult<IEnumerable<Claim>> Get(string tipo, int id)
-		{
-			try{
-				if(tipo == "Baggage"){
-					ClaimConnection conec= new ClaimConnection();
-					List<Claim> claimList = conec.GetClaimBaggage(id);
-					return Ok(claimList); 
-				}else{
-					if(tipo == "documentPasaport"){                   
-						ClaimConnection conec= new ClaimConnection();
-						List<Claim> claimList = conec.GetClaimDocumentPasaport(id);
-						return Ok(claimList); 
-					}else{
-						if(tipo == "documentCedula"){                   
-							ClaimConnection conec= new ClaimConnection();
-							List<Claim> claimList = conec.GetClaimDocumentCedula(id);
-							return Ok(claimList); 
-						}else                
-							return StatusCode(404);
-					}                            
-				}
-			}catch (DatabaseException )
-			{            
-				return StatusCode(500);
-			}
-			catch (GeneralException )
-			{
-				return StatusCode(500);
-			}
-		}
-		// Post api/Claim/
-	
-		[HttpPost]
-		public ActionResult<string> Post([FromBody] ClaimSecundary ClaimAux)
-		{            
-			try
-			{ 				
-				ClaimConnection conec= new ClaimConnection();
-				Claim claim= new Claim(ClaimAux.title, ClaimAux.description, ClaimAux.status);
-				conec.AddClaim(claim);
-				return Ok("Agregado correctamente");
-			}catch (DatabaseException )
-			{            
-				return StatusCode(500);
-			}
-			catch (GeneralException )
-			{
-				return StatusCode(500);
-			}
-
-		}
-		
-		// DELETE api/Claim/5
-		[HttpDelete("{id}")]
-		public ActionResult<string> Delete(int id)
-		{
-			try{
-				ClaimConnection conec= new ClaimConnection();  
-				conec.DeleteClaim(id);
-				return Ok("Eliminado exitosamente");
-			}catch (DatabaseException )
-			{            
-				return StatusCode(500);
-			}
-			catch (GeneralException )
-			{
-				return StatusCode(500);
-			}
-
-		}
-		
-		//api/Clain/status/5
-		[HttpPut("{id}")]
-		public ActionResult<string> Put(int id,[FromBody] ClaimSecundary ClaimAux)
-		{
-			try{
-				ClaimConnection conec = new ClaimConnection();
-				Claim claim = new Claim(ClaimAux.title, ClaimAux.description, ClaimAux.status);
-				Console.WriteLine("estoy aqui");
-				if (ClaimAux.status != null)
-					conec.ModifyClaimStatus(id, claim);
-				else if (ClaimAux.title != null && ClaimAux.description  != null)
-					conec.ModifyClaimTitle(id, claim);
-				return Ok("Modificado exitosamente");
-			}catch (DatabaseException )
-			{            
-				return StatusCode(500);
-			}
-			catch (GeneralException )
-			{
-				return StatusCode(500);
-			} 
-		}
+     [Produces("application/json")]
+     [Route("api/[controller]")]
+     [EnableCors("MyPolicy")]
+     [ApiController]
+     public class BaggageController : ControllerBase
+     {
 
 
-	}
+          // GET api/values
+          //se usara para consultar por pasaporte
+          [HttpGet]
+          public ActionResult<IEnumerable<Baggage>> GetBaggage()
+          {
 
-	public class BaggageSecundary
-    {
+               var baggage = new List<Baggage>();
 
-        private string MaletaId;
-        private string MaletaFkVuelo;
-        private string MaletaFkCrucero;
-        private string MaletaStatus;
-        private string CantidadPasajeros;
-        private string Descripcion; 
+               try
+               {
+                    baggage = BaggageConnection.GetBaggage();
+               }
+               catch (DatabaseException)
+               {
+                    return BadRequest("Error obteniendo los empleados");
+               }
+               return baggage;
 
-      
-	}
+          }
+
+
+
+
+     }
+
+     public class BaggageSecundary
+     {
+
+          private string MaletaId;
+          private string MaletaFkVuelo;
+          private string MaletaFkCrucero;
+          private string MaletaStatus;
+          private string CantidadPasajeros;
+          private string Descripcion;
+
+
+     }
 }
