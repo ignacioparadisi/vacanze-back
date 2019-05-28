d61006cf74373261d8aba75dabe86fb10876b7d1
Juan Goncalves <juan.goncalves498@gmail.com>
2019-05-28 19:13:03 -0400

    refactor: actualizar clase Hotel para usar todos los atributos que tiene en la BD feat: agregadas validaciones basicas en entidad Hotel

--- VacanzeApi/Common/Entities/Grupo6/Hotel.cs
+++ VacanzeApi/Common/Entities/Grupo6/Hotel.cs
@@ -1,37 +1,45 @@
-using Newtonsoft.Json;
+using vacanze_back.VacanzeApi.Common.Exceptions;
 
 namespace vacanze_back.VacanzeApi.Common.Entities.Grupo6
 {
     public class Hotel : Entity
     {
-        // TODO: Cuando se cree la clase Lugar, implementar esta parte
-        // private Lugar _direccion;
+        // [JsonConstructor]
 
-        [JsonConstructor]
-        public Hotel(long id, string name, int amountOfRooms, bool isActive,
-            string phone, string website) : base(id)
+        public Hotel() : base(0)
         {
-            Name = name;
-            AmountOfRooms = amountOfRooms;
-            IsActive = isActive;
-            Phone = phone;
-            Website = website;
         }
 
-        public Hotel(string name, int amountOfRooms, bool isActive,
-            string phone, string website) : base(0)
-        {
-            Name = name;
-            AmountOfRooms = amountOfRooms;
-            IsActive = isActive;
-            Phone = phone;
-            Website = website;
-        }
+        public string Name { get; set; }
+        public int AmountOfRooms { get; set; }
+        public int RoomCapacity { get; set; }
+        public bool IsActive { get; set; }
+        public string AddressSpecification { get; set; }
+        public decimal PricePerRoom { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public string Picture { get; set; }
+        public int Stars { get; set; }
+        public Location Location { get; set; }
 
-        public string Name { get; }
-        public int AmountOfRooms { get; }
-        public bool IsActive { get; }
-        public string Phone { get; }
-        public string Website { get; }
+        public void Validate()
+        {
+            if (Name == null)
+                throw new HotelDataException(
+                    "El nombre del hotel no fue especificado y es requerido");
+            if (AddressSpecification == null)
+                throw new HotelDataException("La direccion especifica del hotel es necesaria");
+            if (Location == null)
+                throw new HotelDataException("La ubicacion del hotel es necesaria");
+            if (AmountOfRooms <= 0)
+                throw new HotelDataException("La cantidad de habitaciones debe ser mayor a 1");
+            if (RoomCapacity <= 0)
+                throw new HotelDataException(
+                    "La capacidad de personas por habitacion debe ser mayor a 1");
+            if (Stars < 1 || Stars > 5)
+                throw new HotelDataException("La cantidad de estrellas debe estar entre 1 y 5");
+            if (PricePerRoom < 0)
+                throw new HotelDataException("El precio por habitacion no puede ser negativo");
+        }
     }
 }
