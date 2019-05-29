4d33819b54d0b5a74fb682aacee0ea7a33127962
Yisusrly <45052243+Yisusrly@users.noreply.github.com>
2019-05-28 22:04:12 -0400

    arreglando el orden del codigo en connectAuto.cs

--- VacanzeApi/Persistence/Repository/Grupo5/ConnectAuto.cs
+++ VacanzeApi/Persistence/Repository/Grupo5/ConnectAuto.cs
@@ -25,26 +25,22 @@ namespace vacanze_back.VacanzeApi.Persistence.Repository.Grupo5
             string command = "MODIFYAUTOMOBILE (@AUT_ID,@AUT_MAKE,@AUT_MODEL,@AUT_CAPACITY,@AUT_ISACTIVE,@AUT_LICENSE,@AUT_PRICE,@AUT_PICTURE,@AUT_LOC_FK)";
             var table = PgConnection.Instance.ExecuteFunction(
                 command,auto.getId(),auto.getmake(),auto.getmodel(),auto.getcapacity(),auto.getisActive(),auto.getlicence(),auto.getprice(), auto.getpicture()  ,auto.getplace());
-        var id = Convert.ToInt32(table.Rows[0][0]);
-          return id;
-
+            var id = Convert.ToInt32(table.Rows[0][0]);
+            return id;
         }
-       public static int DeleteAuto ( int id){
+
+       public static int DeleteAuto (int id){
            try{   
-        string command="DeleteAuto (@AUT_ID)";
-        var table = PgConnection.Instance.ExecuteFunction(command,id);
-        
-        return id;
-                    }
-            catch (InvalidCastException)
-            {
+                string command="DeleteAuto (@AUT_ID)";
+                var table = PgConnection.Instance.ExecuteFunction(command,id);
+                return id;
+            }catch (InvalidCastException){
                 return -1;
-            }
-            catch (Exception ){
+            }catch (Exception ){
                 return id;
             }
-            
         }
+
        public static List<Auto> ConsultforId(int idconsulta){
             var AutoList = new List<Auto>();
             bool isactive;
@@ -54,24 +50,21 @@ namespace vacanze_back.VacanzeApi.Persistence.Repository.Grupo5
             {
                 var id = Convert.ToInt32(table.Rows[i][0]);
                 var make = table.Rows[i][1].ToString();
-                    var model = table.Rows[i][2].ToString();
-                    var capacity =Convert.ToInt32(table.Rows[i][3]);
-                    var status = table.Rows[i][4].ToString();
-                    var price = Convert.ToInt32(table.Rows[i][5]);
-                    var licence = table.Rows[i][6].ToString();
-                    var picture =table.Rows[i][7].ToString();
-                    var place =Convert.ToInt32(table.Rows[i][0]);
-                    if (status== "true"){
-                        isactive=true;
-                    }else {isactive= false;}
-                    Auto auto=new Auto(make,model,capacity,isactive,licence,price,picture,place);
-                    auto.setId(id);
-                    AutoList.Add(auto);
+                var model = table.Rows[i][2].ToString();
+                var capacity =Convert.ToInt32(table.Rows[i][3]);
+                var status = table.Rows[i][4].ToString();
+                var price = Convert.ToInt32(table.Rows[i][5]);
+                var licence = table.Rows[i][6].ToString();
+                var picture =table.Rows[i][7].ToString();
+                var place =Convert.ToInt32(table.Rows[i][0]);
+                if (status== "true"){
+                    isactive=true;
+                }else {isactive= false;}
+                Auto auto=new Auto(make,model,capacity,isactive,licence,price,picture,place);
+                auto.setId(id);
+                AutoList.Add(auto);
             }
-
             return AutoList;
         }
-           
-
     }
 }
