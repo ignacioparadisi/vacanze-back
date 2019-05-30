db2431a70894fb6e79bfb1500206bcc5ee2d42c1
zilus13 <luisgermenher@gmail.com>
2019-05-30 15:28:49 -0400

    resolviendo los conflictos

--- VacanzeApi/Persistence/Repository/Grupo4/BaggageConnection.cs
+++ VacanzeApi/Persistence/Repository/Grupo4/BaggageConnection.cs
@@ -4,19 +4,10 @@ using Npgsql;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo4;
 using vacanze_back.VacanzeApi.Common.Exceptions;
 
-<<<<<<< HEAD
 namespace vacanze_back.VacanzeApi.Persistence.Grupo4
 {
      public class BaggageConnection
      {
-=======
-namespace vacanze_back.VacanzeApi.Persistence.Connection.Grupo4
-{
-     public class BaggageConnection
-     {
-
-
->>>>>>> 979d953386630d498c24041e0c89ee6e1993d0bc
           /// <summary>
           ///     Metodo para agregar un Baggage
           /// </summary>
@@ -36,11 +27,7 @@ namespace vacanze_back.VacanzeApi.Persistence.Connection.Grupo4
 
 
 
-<<<<<<< HEAD
           public void DeleteBaggage(int baggageId)
-=======
-          public void DeleteBaggage(long baggageId)
->>>>>>> 979d953386630d498c24041e0c89ee6e1993d0bc
           {
 
                var table = PgConnection.Instance.ExecuteFunction("DeleteBaggage(@bag_id)",
@@ -48,26 +35,16 @@ namespace vacanze_back.VacanzeApi.Persistence.Connection.Grupo4
 
           }
 
-<<<<<<< HEAD
           public void ModifyBaggageStatus(int baggageId, Baggage baggage)
-=======
-          public void ModifyBaggageStatus(long baggageId, Baggage baggage)
->>>>>>> 979d953386630d498c24041e0c89ee6e1993d0bc
           {
                var table = PgConnection.Instance.ExecuteFunction("modifyBaggageStatus(@bag_id,@bag_status)",
                   baggageId, baggage.MaletaStatus);
 
           }
 
-<<<<<<< HEAD
           public void ModifyBaggageDescripcion(int baggageId, Baggage baggage)
           {
                var table = PgConnection.Instance.ExecuteFunction("modifyBaggageDescri(@bag_id, @bag_descr)",
-=======
-          public void ModifyBaggageDescripcion(long baggageId, Baggage baggage)
-          {
-               var table = PgConnection.Instance.ExecuteFunction("modifyclaimtitle(@bag_id, @bag_descr)",
->>>>>>> 979d953386630d498c24041e0c89ee6e1993d0bc
                  baggageId, baggage.Descripcion);
           }
 
@@ -77,15 +54,9 @@ namespace vacanze_back.VacanzeApi.Persistence.Connection.Grupo4
                var BaggageList = new List<Baggage>();
                for (var i = 0; i < table.Rows.Count; i++)
                {
-<<<<<<< HEAD
                     var id = Convert.ToInt32(table.Rows[i][0]);
                     var Vuelo = Convert.ToInt32(table.Rows[i][1]); ;
                     var Crucero = Convert.ToInt32(table.Rows[i][2]); ;
-=======
-                    var id = Convert.ToInt64(table.Rows[i][0]);
-                    var Vuelo = Convert.ToInt64(table.Rows[i][1]); ;
-                    var Crucero = Convert.ToInt64(table.Rows[i][2]); ;
->>>>>>> 979d953386630d498c24041e0c89ee6e1993d0bc
                     var descripcion = table.Rows[i][4].ToString();
                     var status = table.Rows[i][3].ToString();
 
