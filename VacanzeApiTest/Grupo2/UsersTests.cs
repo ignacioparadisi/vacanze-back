81727fee89bb2c563a825fa167c019f89266a0ff
gabrielbaron16 <gabrielbu161@gmail.com>
2019-05-30 21:26:20 -0400

    Pruebas para Caso de Uso Obtener Usuario por Id

--- VacanzeApiTest/Grupo2/UsersTests.cs
+++ VacanzeApiTest/Grupo2/UsersTests.cs
@@ -127,7 +127,21 @@ namespace vacanze_back.VacanzeApiTest.Grupo2
             var user = UserRepository.AddUser(UserTest);
             Assert.Throws<RepeatedEmailException>(() => UserRepository.VerifyEmail("cliente1@vacanze.com"));
         }
-        
+
+        [Test]
+        public void GetUserTest()
+        {
+            var controller = new UsersController();
+            ActionResult<User> user = controller.Get(1);
+            Assert.True((user.Value.Id == 1) && (user.Value.Roles.Count > 0));
+        }
+
+        [Test]
+        public void UserNotFoundTest()
+        {
+            var controller = new UsersController();
+            Assert.Throws<UserNotFoundException>(() => UserRepository.GetUserById(100));
+        }
 
         [TearDown]
         public void DeleteUserTest()
