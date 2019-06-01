ffd83bd52e482e53ed799ea7c518e0fbde826a5f
cesarbonadio <cesarbonadio123@gmail.com>
2019-05-31 22:15:27 -0400

    feat: pruebas de controlador de hoteles

---
+++ VacanzeApiTest/Grupo6/HotelControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo6;
+using vacanze_back.VacanzeApi.Services.Controllers.Grupo6;
+using vacanze_back.VacanzeApi.Common.Exceptions;
+
+namespace vacanze_back.VacanzeApiTest.Grupo6
+{
+    [TestFixture]
+    public class HotelControllerTests
+    {
+        private HotelsController hotelsController;
+        [SetUp]
+        public void Setup()
+        {
+            hotelsController = new HotelsController();
+        }
+        [Test]
+        public void GetHotelsTest()
+        {
+            ActionResult<IEnumerable<Hotel>> hotels = hotelsController.Get();
+            Assert.NotNull(hotels);
+        }
+        [Test]
+        public void GetHotelsByLocationTest()
+        {
+            ActionResult<IEnumerable<Hotel>> hotels = hotelsController.Get(1);
+            Assert.NotNull(hotels.Value);
+        }
+        [Test]
+        public void GetHotelById()
+        {
+            ActionResult<Hotel> hotel = hotelsController.GetById(2);
+            Assert.NotNull(hotel.Value);
+        }
+        [Test]
+        public void GetNotExistingHotelById()
+        {
+            ActionResult<Hotel> hotel = hotelsController.GetById(1000);
+            Assert.IsInstanceOf<NotFoundResult>(hotel.GetType());
+        }
+        [Test]
+        public void GetHotelsEqualsLocationNullTest()
+        {
+            ActionResult<IEnumerable<Hotel>> hotels = hotelsController.Get();
+            ActionResult<IEnumerable<Hotel>> _hotels = hotelsController.Get(-1);
+            Assert.AreEqual(hotels.Value.ToString(), _hotels.Value.ToString());
+        }
+        
+
+        
+    }
+
+}
