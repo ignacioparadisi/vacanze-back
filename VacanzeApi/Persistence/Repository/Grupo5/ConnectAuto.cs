3e0a782248fed7794694bbfb8dbae2770a22d423
Yisusrly <45052243+Yisusrly@users.noreply.github.com>
2019-06-01 11:13:09 -0400

    acomodando presentacion del codigo y cubriendo con try-catch basico, parte 1

--- VacanzeApi/Persistence/Repository/Grupo5/ConnectAuto.cs
+++ VacanzeApi/Persistence/Repository/Grupo5/ConnectAuto.cs
@@ -1,46 +1,64 @@
 using System;
 using System.Collections.Generic;
-using Npgsql;      
+using Npgsql;
+using Microsoft.AspNetCore.Mvc;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo5;
+using vacanze_back.VacanzeApi.Common.Exceptions;
 
 namespace vacanze_back.VacanzeApi.Persistence.Repository.Grupo5
 {
     public static class ConnectAuto 
     {
-
-       public static int  Agregar(Auto auto)
+       public static int Agregar(Auto auto)
         {
-            
-            String command ="ADDAUTOMOBILE (@AUT_MAKE,@AUT_MODEL,@AUT_CAPACITY,@AUT_ISACTIVE,@AUT_LICENSE,@AUT_PRICE,@AUT_PICTURE,@AUT_LOC_FK)";
-            var table = PgConnection.Instance.ExecuteFunction(
-                command,auto.getmake(),auto.getmodel(),auto.getcapacity(),auto.getisActive(), auto.getlicence(),auto.getprice(), auto.getpicture(),auto.getplace());
-                
-            var id = Convert.ToInt32(table.Rows[0][0]);
-          return id;
-       
+            try
+            {
+                String command ="ADDAUTOMOBILE (@AUT_MAKE,@AUT_MODEL,@AUT_CAPACITY,@AUT_ISACTIVE,@AUT_LICENSE,@AUT_PRICE,@AUT_PICTURE,@AUT_LOC_FK)";
+                var table = PgConnection.Instance.ExecuteFunction(
+                    command,auto.getmake(),auto.getmodel(),auto.getcapacity(),auto.getisActive(), 
+                    auto.getlicence(),auto.getprice(), auto.getpicture(),auto.getplace());
+                var id = Convert.ToInt32(table.Rows[0][0]);
+                return id;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
-        
        public static int ModifyAuto (Auto auto)
         {
-            string command = "MODIFYAUTOMOBILE (@AUT_ID,@AUT_MAKE,@AUT_MODEL,@AUT_CAPACITY,@AUT_ISACTIVE,@AUT_LICENSE,@AUT_PRICE,@AUT_PICTURE,@AUT_LOC_FK)";
-            var table = PgConnection.Instance.ExecuteFunction(
-                command,auto.getId(),auto.getmake(),auto.getmodel(),auto.getcapacity(),auto.getisActive(),auto.getlicence(),auto.getprice(), auto.getpicture()  ,auto.getplace());
-            var id = Convert.ToInt32(table.Rows[0][0]);
-            return id;
+            try
+            {
+                string command = "MODIFYAUTOMOBILE (@AUT_ID,@AUT_MAKE,@AUT_MODEL,@AUT_CAPACITY,@AUT_ISACTIVE,@AUT_LICENSE,@AUT_PRICE,@AUT_PICTURE,@AUT_LOC_FK)";
+                var table = PgConnection.Instance.ExecuteFunction(
+                    command,auto.getId(),auto.getmake(),auto.getmodel(),auto.getcapacity(),auto.getisActive(),
+                    auto.getlicence(),auto.getprice(),auto.getpicture(),auto.getplace());
+                var id = Convert.ToInt32(table.Rows[0][0]);
+                return id;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
-
        public static int DeleteAuto (int id){
-           try{   
+           try
+           {   
                 string command="DeleteAuto (@AUT_ID)";
                 var table = PgConnection.Instance.ExecuteFunction(command,id);
                 return id;
-            }catch (InvalidCastException){
+            }
+            catch(InvalidCastException)
+            {
                 return -1;
-            }catch (Exception ){
+            }
+            catch (Exception)
+            {
                 return id;
             }
         }
-
        public static List<Auto> ConsultforId(int idconsulta){
             var AutoList = new List<Auto>();
             bool isactive;
@@ -57,14 +75,12 @@ namespace vacanze_back.VacanzeApi.Persistence.Repository.Grupo5
                 var licence = table.Rows[i][6].ToString();
                 var picture =table.Rows[i][7].ToString();
                 var place =Convert.ToInt32(table.Rows[i][8]);
-               
                 Auto auto=new Auto(make,model,capacity,status,licence,price,picture,place);
                 auto.setId(id);
                 AutoList.Add(auto);
             }
             return AutoList;
         }
-           
         public static List<Auto> consultforall(int _place , string _result , string _license , int _capacity)
         {
             var AutoList = new List<Auto>();
@@ -75,20 +91,18 @@ namespace vacanze_back.VacanzeApi.Persistence.Repository.Grupo5
             {
                 var id = Convert.ToInt32(table.Rows[i][0]);
                 var make = table.Rows[i][1].ToString();
-                    var model = table.Rows[i][2].ToString();
-                    var capacity =Convert.ToInt32(table.Rows[i][3]);
-                    var status = Convert.ToBoolean(table.Rows[i][4]);
-                    var price = Convert.ToInt32(table.Rows[i][5]);
-                    var licence = table.Rows[i][6].ToString();
-                    var picture =table.Rows[i][7].ToString();
-                    var place =Convert.ToInt32(table.Rows[i][8]);
-               
-                    Auto auto=new Auto(make,model,capacity,status,licence,price,picture,place);
-                    auto.setId(id);
-                    AutoList.Add(auto);
+                var model = table.Rows[i][2].ToString();
+                var capacity =Convert.ToInt32(table.Rows[i][3]);
+                var status = Convert.ToBoolean(table.Rows[i][4]);
+                var price = Convert.ToInt32(table.Rows[i][5]);
+                var licence = table.Rows[i][6].ToString();
+                var picture =table.Rows[i][7].ToString();
+                var place =Convert.ToInt32(table.Rows[i][8]);
+                Auto auto=new Auto(make,model,capacity,status,licence,price,picture,place);
+                auto.setId(id);
+                AutoList.Add(auto);
             }
             return AutoList;
-
         } 
          public static List<Auto> ConsultforPlaceandStatu(int _place,bool _status)
         {
@@ -107,38 +121,34 @@ namespace vacanze_back.VacanzeApi.Persistence.Repository.Grupo5
                 var licence = table.Rows[i][6].ToString();
                 var picture =table.Rows[i][7].ToString();
                 var place =Convert.ToInt32(table.Rows[i][8]);
-
                 Auto auto=new Auto(make,model,capacity,_status,licence,price,picture,place);
                 auto.setId(id);
                 AutoList.Add(auto);
             }
             return AutoList;
         }        
-
-
            public static List<Auto> ConsultforPlace(int _id)
         {
             var AutoList = new List<Auto>();
             bool isactive;
             string command="ConsultforPlaceAuto (@AUT_PLACE)";
-             var table = PgConnection.Instance.ExecuteFunction(command,_id);
-             for (int i = 0; i < table.Rows.Count; i++)
+            var table = PgConnection.Instance.ExecuteFunction(command,_id);
+            for (int i = 0; i < table.Rows.Count; i++)
             {
                 var id = Convert.ToInt32(table.Rows[i][0]);
                 var make = table.Rows[i][1].ToString();
-                    var model = table.Rows[i][2].ToString();
-                    var capacity =Convert.ToInt32(table.Rows[i][3]);
-                    var status = Convert.ToBoolean(table.Rows[i][4]);
-                    var price = Convert.ToInt32(table.Rows[i][5]);
-                    var licence = table.Rows[i][6].ToString();
-                    var picture =table.Rows[i][7].ToString();
-                    var place =Convert.ToInt32(table.Rows[i][8]);
-                    Auto auto=new Auto(make,model,capacity,status,licence,price,picture,place);
-                    auto.setId(id);
-                    AutoList.Add(auto);
+                var model = table.Rows[i][2].ToString();
+                var capacity =Convert.ToInt32(table.Rows[i][3]);
+                var status = Convert.ToBoolean(table.Rows[i][4]);
+                var price = Convert.ToInt32(table.Rows[i][5]);
+                var licence = table.Rows[i][6].ToString();
+                var picture =table.Rows[i][7].ToString();
+                var place =Convert.ToInt32(table.Rows[i][8]);
+                Auto auto=new Auto(make,model,capacity,status,licence,price,picture,place);
+                auto.setId(id);
+                AutoList.Add(auto);
             }
             return AutoList;
-
         }
     }                    
 }
--- VacanzeApi/Services/Controllers/Grupo5/AutoController.cs
+++ VacanzeApi/Services/Controllers/Grupo5/AutoController.cs
@@ -19,53 +19,53 @@ namespace vacanze_back.VacanzeApi.Services.Controllers.Grupo5
         [HttpGet("agregar/{make}/{model}/{capacity}/{status}/{licence}/{price}/{picture}/{place}")]
         public ActionResult<IEnumerable<String>> agregar(string make,string model,int capacity,bool status, string licence, float price, string picture , int place)
         {
-            try {
-            Auto auto = new Auto(make,model,capacity,status,licence,price,picture,place);
-            var result= ConnectAuto.Agregar(auto);
-            return Ok(JsonConvert.SerializeObject(result));
-
-            }catch (IndexOutOfRangeException)
+            try 
+            {
+                Auto auto = new Auto(make,model,capacity,status,licence,price,picture,place);
+                var result= ConnectAuto.Agregar(auto);
+                return Ok(JsonConvert.SerializeObject(result));
+            }
+            catch (IndexOutOfRangeException)
             {
                 return StatusCode(500,"no se registro carro");
-            }
-;
+            }/*;*/
         }
         /*https://localhost:5001/api/Auto/eliminar/2*/
         [HttpGet("eliminar/{i}")]
         public IActionResult delete(int i)
         {
-        
             var result= ConnectAuto.DeleteAuto(i);
               if (result.Equals(-1))
             {
                 return StatusCode(500, "El automovil no existe");
             }
             return StatusCode(200, "Eliminado satisfactoriamente");
-        
         }
         /*https://localhost:5001/api/Auto/consultarforall/2 
          result = isactive */ 
         [HttpGet("consultforall/{place}/{result}/{license}/{capacity}")]
         public  ActionResult<IEnumerable<Auto>> GetconsultAll(int place , string result , string license , int capacity)
         {
-            try{
-                
+            try
+            { 
                 List<Auto> AutoList =ConnectAuto.consultforall(place,result,license,capacity);
-                 return Ok(JsonConvert.SerializeObject(AutoList));
-            }catch (IndexOutOfRangeException)
+                return Ok(JsonConvert.SerializeObject(AutoList));
+            }
+            catch (IndexOutOfRangeException)
             {
                 return StatusCode(500,"No hay auto registrados");
             }
         }
-         /*https://localhost:5001/api/Auto/consultforplace/1 */
+        /*https://localhost:5001/api/Auto/consultforplace/1 */
         [HttpGet("consultforplace/{id}")]
         public  ActionResult<IEnumerable<Auto>> GetconsultPlace(int id)
         {
-            try{
-                
+            try
+            {    
                 List<Auto> AutoList =ConnectAuto.ConsultforPlace(id);
-                 return Ok(JsonConvert.SerializeObject(AutoList));
-            }catch (IndexOutOfRangeException)
+                return Ok(JsonConvert.SerializeObject(AutoList));
+            }
+            catch (IndexOutOfRangeException)
             {
                 return StatusCode(500,"No hay auto registrados");
             }
@@ -74,13 +74,12 @@ namespace vacanze_back.VacanzeApi.Services.Controllers.Grupo5
          [HttpGet("consult/{id}")]
         public  ActionResult<IEnumerable<Auto>> Getconsulforid(int id )
         {
-            try{
-                
+            try
+            {    
                 List<Auto> AutoList =ConnectAuto.ConsultforId(id);
-                
-                 return Ok(AutoList.ToList());
-                 
-            }catch (IndexOutOfRangeException)
+                return Ok(AutoList.ToList());    
+            }
+            catch (IndexOutOfRangeException)
             {
                 return Ok("no hay autos registrados");
             }
@@ -89,29 +88,28 @@ namespace vacanze_back.VacanzeApi.Services.Controllers.Grupo5
         [HttpGet("consultplaceStatus/{place}/{status}")]
         public  ActionResult<IEnumerable<Auto>> GetConsultforPlaceandStatu(int place, bool status)
         {
-            try{
-                
+            try
+            {    
                 List<Auto> AutoList =ConnectAuto.ConsultforPlaceandStatu(place,status);
-                 return Ok(JsonConvert.SerializeObject(AutoList));
-            }catch (IndexOutOfRangeException)
+                return Ok(JsonConvert.SerializeObject(AutoList));
+            }
+            catch (IndexOutOfRangeException)
             {
                 return StatusCode(500,"No hay auto registrados");
             }
         }
-
-
-
 		/*https://localhost:5001/api/Auto/modificar/2/cambio/cambio/532/false/cambio/12345/cambio/2 */
         [HttpGet("modificar/{id}/{make}/{model}/{capacity}/{status}/{licence}/{price}/{picture}/{place}")]
         public  IActionResult  modify(int id, string make,string model,int capacity,bool status, string licence, float price, string picture , int place)
         {
-            try {
-            Auto auto = new Auto(make,model,capacity,status,licence,price,picture,place);
-            auto.setId(id);
-            var result= ConnectAuto.ModifyAuto(auto);
-            return Ok(JsonConvert.SerializeObject(result));
-
-            }catch (IndexOutOfRangeException)
+            try 
+            {
+                Auto auto = new Auto(make,model,capacity,status,licence,price,picture,place);
+                auto.setId(id);
+                var result= ConnectAuto.ModifyAuto(auto);
+                return Ok(JsonConvert.SerializeObject(result));
+            }
+            catch (IndexOutOfRangeException)
             {
                 return StatusCode(500,"no se registro carro");
             }
