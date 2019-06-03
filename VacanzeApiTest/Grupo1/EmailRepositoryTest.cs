d74eb340c7104f3c5914c3bd5812162224570445
Jhonni Calle <jhonnicalle@gmail.com>
2019-06-03 12:47:45 -0400

    u test added

---
+++ VacanzeApiTest/Grupo1/EmailRepositoryTest.cs
@@ -0,0 +1,38 @@
+﻿namespace vacanze_back.VacanzeApiTest.Grupo1
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using vacanze_back.VacanzeApi.Persistence.Repository.Grupo1;
+
+    [TestFixture]
+    public class EmailRepositoryTest
+    {
+        EmailRepository emailRepository;
+        [SetUp]
+        public void SetUp()
+        {
+            emailRepository = new EmailRepository();
+        }
+
+        [Test]
+        public void EmailRecoverySuccess()
+        {
+            var email = emailRepository.Recovery("admin@vacanze.com");
+
+            Assert.IsNotNull(email);
+        }
+
+        [Test]
+        public void EmailRecoveryFailed()
+        {
+            var email = emailRepository.Recovery("admin@vacanze.com");
+
+            Assert.IsNotNull(email);
+        }
+
+       
+    }
+}
