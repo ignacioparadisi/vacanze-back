bb34886069a507a9e1aaca65edafd8d69ac5ab35
zilus13 <luisgermenher@gmail.com>
2019-06-02 23:18:30 -0400

    arregladon el API de grupo4 y agregando las pruebas

---
+++ VacanzeApiTest/Grupo4/BaggagesTest.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo4;
+using vacanze_back.VacanzeApi.Services.Controllers.Grupo4;
+using vacanze_back.VacanzeApi.Persistence.Repository.Grupo4;
+using vacanze_back.VacanzeApi.Common.Exceptions;
+
+namespace vacanze_back.VacanzeApiTest.Grupo4
+{
+     [TestFixture]
+     public class BaggageTest
+     {
+          ActionResult<IEnumerable<Baggage>> Baggage;
+          BaggageController controller;
+
+          BaggageRepository conec;
+          int response;
+          [SetUp]
+          public void setup()
+          {
+               controller = new BaggageController();
+               conec = new BaggageRepository();
+          }
+
+
+          [Test]
+          public void BaggageDeleteByIdTest()
+          {
+               var response = BaggageRepository.AddBaggageReturnId(3, 3, "Maleta verde", "EXTRAVIADO");
+               var id = BaggageRepository.DeleteBaggage(response);
+               Assert.AreEqual(id, response);
+          }
+
+          [Test]
+          public void AddBaggageTest()
+          {
+               var response = BaggageRepository.AddBaggage(3, 3, "Maleta verde", "EXTRAVIADO");
+               Assert.AreEqual(response, "Maleta Agregada");
+          }
+
+     }
+
+
+
+}
