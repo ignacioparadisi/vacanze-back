6c0f7227bc319228b9dea3a1b0e66a350c6f0652
Gino Martin López <gmlm_120@hotmail.com>
2019-06-27 10:14:43 -0400

    Modificacion de Carpetas con patrones de Modulo 3

---
+++ VacanzeApi/Persistence/DAO/Grupo3/AirplanesRepositoryDAO.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using Npgsql;
+using vacanze_back.VacanzeApi.Common.Entities;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo3;
+using vacanze_back.VacanzeApi.Common.Exceptions;
+using vacanze_back.VacanzeApi.Common.Exceptions.Grupo3;
+
+
+namespace vacanze_back.VacanzeApi.Persistence.DAO.Grupo3
+{
+    public static class AirplanesRepositoryDAO
+    {
+        static string GET_ALL_PLANES = "getplanes()";
+        static string FIND_PLANE = "findplane(@_id)";
+
+        /// <summary>Busca aviones en la DB</summary>
+        /// <returns> List<Entity> con el resultado de la query </returns>
+        public static List<Entity> Get()
+        {
+            try
+            {
+                var table = PgConnection.Instance.ExecuteFunction(GET_ALL_PLANES);
+                List<Entity> airplanes = new List<Entity>();
+
+                for (var i = 0; i < table.Rows.Count; i++)
+                {
+                    Airplane airplane = new Airplane(Convert.ToInt32(table.Rows[i][0]));
+                    airplane.autonomy = Convert.ToInt32(table.Rows[i][1]);
+                    airplane.isActive = Convert.ToBoolean(table.Rows[i][2]);
+                    airplane.seats = Convert.ToInt32(table.Rows[i][3]);
+                    airplane.loadCapacity = Convert.ToInt32(table.Rows[i][4]);
+                    airplane.model = table.Rows[i][5].ToString();
+
+                    airplanes.Add(airplane);
+                }
+
+                return airplanes;
+            }
+            catch (DatabaseException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new DbErrorException(
+                    "Ups, a ocurrido un error al conectarse a la base de datos", ex);
+            }
+            finally
+            {
+            }
+        }
+
+        /// <summary>Busca vuelo especifico en la DB</summary>
+        /// <param name="id">Id del avion a buscar</param>
+        /// <returns>Entity con el resultado de la query</returns>
+        public static Entity Find(long id)
+        {
+            try
+            {
+                Airplane airplane = null;
+                var table = PgConnection.Instance.ExecuteFunction(FIND_PLANE,(int)id);
+
+                for (var i = 0; i < table.Rows.Count; i++)
+                {
+                    airplane = new Airplane(Convert.ToInt32(table.Rows[i][0]));
+                    airplane.autonomy = Convert.ToInt32(table.Rows[i][1]);
+                    airplane.isActive = Convert.ToBoolean(table.Rows[i][2]);
+                    airplane.seats = Convert.ToInt32(table.Rows[i][3]);
+                    airplane.loadCapacity = Convert.ToInt32(table.Rows[i][4]);
+                    airplane.model = table.Rows[i][5].ToString();
+
+                }
+                return airplane;
+            }
+            catch (DatabaseException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new DbErrorException(
+                    "Ups, a ocurrido un error al conectarse a la base de datos", ex);
+            }
+            finally
+            {
+            }
+        }
+    }
+}
---
+++ VacanzeApi/Persistence/DAO/Grupo3/FlightsRepositoryDAO.cs
@@ -0,0 +1,348 @@
+using System;
+using System.Collections.Generic;
+using Npgsql;
+using vacanze_back.VacanzeApi.Common.Entities;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo3;
+using vacanze_back.VacanzeApi.Common.Exceptions;
+using vacanze_back.VacanzeApi.Common.Exceptions.Grupo3;
+using System.Data;
+
+
+namespace vacanze_back.VacanzeApi.Persistence.DAO.Grupo3
+{
+    public static class FlightRepositoryDAO
+    {
+        static string GET_ALL_FLIGHTS = "getflights()";
+        static string ADD_FLIGHT =
+        "addflight( @_plane, @_price, @_departure,@_arrival, @_loc_arrival, @_loc_departure )";
+        static string GET_FLIGTS_BY_DATE = "getflightsbydate(@_begin, @_end)";
+        static string GET_FLIGHTS_BY_LOCATION = "getflightsbylocation(@_departure, @_arrival)";
+        static string FIND_FLIGHT = "findflight(@_id)";
+        static string UPDATE_FLIGHT =
+        "updateflight(@_id, @_plane, @_price, @_departure, @_arrival, @_loc_departure, @_loc_arrival)";
+        static string DELETE_FLIGHT = "deleteflight(@_id)";
+        static string GET_OUTBOUND_FLIGHTS = "getOutBoundFlights(@_loc_departure, @_loc_arrival, @_departure)";
+        static string GET_ROUNTRIP_FLIGHTS = "getRounTripFlights(@_loc_departure, @_loc_arrival, @_departure, @_arrival)";
+
+
+        /// <summary>Devuelve lista de vuelos de la DB</summary>
+        /// <returns>List<Entity> con el resultado de la query</returns>
+        public static List<Entity> Get()
+        {
+            try
+            {
+                var table = PgConnection.Instance.ExecuteFunction(GET_ALL_FLIGHTS);
+                List<Entity> flights = new List<Entity>();
+
+                for (var i = 0; i < table.Rows.Count; i++)
+                {
+                    Flight flight = GetRow(table,i);
+
+                    flights.Add(flight);
+                }
+
+                return flights;
+            }
+            catch (DatabaseException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new DbErrorException("¡Ups! Hubo un error con la Base de Datos", ex);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+            finally
+            {
+            }
+        }
+
+
+        /// <summary>Agrega vuelo a la DB</summary>
+        /// <param name="entity">Entidad con vuelo a agregar a la DB</param>
+        public static int Add(Entity entity)
+        {
+            try
+            {
+                Flight flight = (Flight)entity;
+
+                var table = PgConnection.Instance.ExecuteFunction(
+                    ADD_FLIGHT,
+                    (int)flight.plane.Id, flight.price, flight.departure, flight.arrival, flight.loc_departure.Id, flight.loc_arrival.Id
+                );
+
+                if (table.Rows.Count > 0){
+                    return Convert.ToInt32(table.Rows[0][0]);
+                }
+
+                return 0;
+
+            }
+            catch (DatabaseException ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Message);
+                throw new DbErrorException("¡Ups! Hubo un error con la Base de Datos", ex);
+            }
+            finally
+            {
+            }
+        }
+
+        /// <summary>Edita vuelo a la DB</summary>
+        /// <param name="entity">Entidad con vuelo a editar en la DB</param>
+        public static void Update(Entity entity)
+        {
+            try
+            {
+                Flight flight = (Flight)entity;
+
+                var table = PgConnection.Instance.ExecuteFunction(
+                    UPDATE_FLIGHT,
+                    (int)flight.Id, (int)flight.plane.Id, flight.price, flight.departure, flight.arrival, (int)flight.loc_departure.Id, (int)flight.loc_arrival.Id
+                );
+            }
+            catch (DatabaseException ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+                throw new DbErrorException("¡Ups! Hubo un error con la Base de Datos", ex);
+            }
+            finally
+            {
+            }
+        }
+
+        /// <summary>Funcion para borrar un vuelo de la DB</summary>
+        /// <param name="entiry">Entidad con id a borrar</param>
+        public static void Delete(Entity entity)
+        {
+            try
+            {
+                Flight flight = (Flight)entity;
+
+                var table = PgConnection.Instance.ExecuteFunction(
+                   DELETE_FLIGHT,
+                   (int)flight.Id
+               );
+
+                // throw new DbErrorException(flight.Id.ToString());
+            }
+            catch (DatabaseException ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+                throw new DbErrorException("¡Ups! Hubo un error con la Base de Datos", ex);
+            }
+            finally
+            {
+            }
+        }
+
+        /// <summary>Busca vuelo especifico en la DB</summary>
+        /// <param name="id">Id del vuelo a busac</param>
+        /// <returns>Entity con el resultado de la query</returns>
+        public static Entity Find(int id)
+        {
+
+            try
+            {
+                var table = PgConnection.Instance.ExecuteFunction(FIND_FLIGHT, id);
+                Flight flight = null;
+
+                if (table.Rows.Count > 0)
+                {
+                    flight = new Flight(Convert.ToInt32(table.Rows[0][0]));
+                    flight.Id = Convert.ToInt32(table.Rows[0][0]);
+                    flight.price = Convert.ToDouble(table.Rows[0][1]);
+                    flight.departure = table.Rows[0][2].ToString();
+                    flight.arrival = table.Rows[0][3].ToString();
+                    flight.loc_departure = LocationDAO.GetLocationById(Convert.ToInt32(table.Rows[0][4]));
+                    flight.loc_arrival = LocationDAO.GetLocationById(Convert.ToInt32(table.Rows[0][5]));
+                    flight.plane = (Airplane)AirplanesRepositoryDAO.Find(Convert.ToInt32(table.Rows[0][6]));
+                }
+
+                return flight;
+
+
+            }
+            catch (DatabaseException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new DbErrorException("¡Ups! Hubo un error con la Base de Datos", ex);
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+            }
+
+        }
+
+        /// <summary>Busca vuelo en un rango de fechas en la DB</summary>
+        /// <param name="begin">Fecha menor a buscar</param>
+        /// <param name="end">Fecha mayor a buscar</param>
+        /// <returns> List<Entity> con el resultado de la query</returns>
+        public static List<Entity> GetByDate(string begin, string end)
+        {
+            try
+            {
+                var table = PgConnection.Instance.ExecuteFunction(GET_FLIGTS_BY_DATE, begin, end);
+                List<Entity> flights = new List<Entity>();
+
+                for (var i = 0; i < table.Rows.Count; i++)
+                {
+
+                    Flight flight = GetRow(table,i);
+
+                    flights.Add(flight);
+                }
+
+                return flights;
+            }
+            catch (DatabaseException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new DbErrorException("¡Ups! Hubo un error con la Base de Datos", ex);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+            finally
+            {
+            }
+        }
+
+        /// <summary>Busca vuelo en un rango de fechas en la DB</summary>
+        /// <param name="departure">Locacion de salida del vuelo</param>
+        /// <param name="arrival">Locacion de llegada del vuelo</param>
+        /// <returns> List<Entity> con el resultado de la query</returns>
+        public static List<Entity> GetByLocation(int departure, int arrival)
+        {
+            try
+            {
+                var table = PgConnection.Instance.ExecuteFunction(GET_FLIGHTS_BY_LOCATION, departure, arrival);
+                List<Entity> flights = new List<Entity>();
+
+                for (var i = 0; i < table.Rows.Count; i++)
+                {
+
+                    Flight flight = GetRow(table,i);
+
+                    flights.Add(flight);
+                }
+
+                return flights;
+            }
+            catch (DatabaseException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new DbErrorException("¡Ups! Hubo un error con la Base de Datos", ex);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+            finally
+            {
+            }
+        }
+
+        /// <summary>Busca por ciudad de salida y llegada y determinada fecha de salida</summary>
+        /// <param name="departure">Ciudad de salida del vuelor</param>
+        /// <param name="arrival">Ciudad de llegada del vuelo</param>
+        /// <param name="departuredate">Fecha de salida del vuelo</param>
+        /// <returns> List<Entity> con el resultado de la query</returns>
+        public static List<Entity> GetOutboundFlights(int departure, int arrival, string departuredate)
+        {
+            try
+            {
+                var table = PgConnection.Instance.ExecuteFunction(GET_OUTBOUND_FLIGHTS, departure, arrival, departuredate);
+                List<Entity> flights = new List<Entity>();
+
+                for (var i = 0; i < table.Rows.Count; i++)
+                {
+
+                    Flight flight = GetRow(table,i);
+                    flights.Add(flight);
+                }
+
+                return flights;
+            }
+            catch (DatabaseException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new DbErrorException("¡Ups! Hubo un error con la Base de Datos", ex);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+            finally
+            {
+            }
+        }
+
+        /// <summary>Busca por ciudad de salida y llegada y determinada fecha de salida y llegada</summary>
+        /// <param name="departure">Ciudad de salida del vuelor</param>
+        /// <param name="arrival">Ciudad de llegada del vuelo</param>
+        /// <param name="departuredate">Fecha de salida del vuelo</param>
+        /// <param name="arrivaldate">Fecha de llegada del vuelo</param>
+        /// <returns> List<Entity> con el resultado de la query</returns>
+        public static List<Entity> GetRoundTripFlights(int departure, int arrival, string departuredate, string arrivaldate)
+        {
+            try
+            {
+                var table = PgConnection.Instance.ExecuteFunction(GET_ROUNTRIP_FLIGHTS, departure, arrival, departuredate, arrivaldate);
+                List<Entity> flights = new List<Entity>();
+
+                for (var i = 0; i < table.Rows.Count; i++)
+                {
+
+                    Flight flight = GetRow(table,i);
+                    flights.Add(flight);
+                }
+
+                return flights;
+            }
+            catch (DatabaseException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new DbErrorException("¡Ups! Hubo un error con la Base de Datos", ex);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+            finally
+            {
+            }
+        }
+
+        static private Flight GetRow(DataTable table, int i){
+
+            Flight flight = new Flight(Convert.ToInt32(table.Rows[i][0]));
+            flight.Id = Convert.ToInt32(table.Rows[i][0]);
+            flight.plane = (Airplane)AirplanesRepositoryDAO.Find(Convert.ToInt32(table.Rows[i][1]));
+            flight.price = Convert.ToDouble(table.Rows[i][2]);
+            flight.departure = table.Rows[i][3].ToString();
+            flight.arrival = table.Rows[i][4].ToString();
+            flight.loc_departure = LocationDAO.GetLocationById(Convert.ToInt32(table.Rows[i][5]));
+            flight.loc_arrival = LocationDAO.GetLocationById(Convert.ToInt32(table.Rows[i][6]));
+
+            return flight;
+
+        }
+
+    }
+}
