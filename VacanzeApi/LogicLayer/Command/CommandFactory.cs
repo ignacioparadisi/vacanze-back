71c7e0be5d6675928fee253b39594b98800fcfb4
cloudyrl <cloudyrl1@gmail.com>
2019-06-30 21:16:03 -0700

    Agregado el comando AddRestaurantCommand para agregar restaurantes

--- VacanzeApi/LogicLayer/Command/CommandFactory.cs
+++ VacanzeApi/LogicLayer/Command/CommandFactory.cs
@@ -3,6 +3,7 @@ using vacanze_back.VacanzeApi.LogicLayer.Command.Grupo5;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo5;
 using System.Collections.Generic;
 using vacanze_back.VacanzeApi.LogicLayer.Command.Grupo7;
+using vacanze_back.VacanzeApi.LogicLayer.DTO.Grupo7;
 
 namespace vacanze_back.VacanzeApi.LogicLayer.Command
 {
@@ -19,6 +20,11 @@ namespace vacanze_back.VacanzeApi.LogicLayer.Command
         public static GetRestaurantCommand CreateGetRestaurantCommand(int id)
         {
             return new GetRestaurantCommand(id);
+        }
+        
+        public static AddRestaurantCommand CreateAddRestaurantCommand(RestaurantDTO restaurantDto)
+        {
+            return new AddRestaurantCommand(restaurantDto);
         }
     }
 }
