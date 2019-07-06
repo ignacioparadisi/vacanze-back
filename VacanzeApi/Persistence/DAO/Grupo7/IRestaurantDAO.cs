24da9294a6c67b901d681073bc7c3239b8c404ef
cloudyrl <cloudyrl1@gmail.com>
2019-07-05 18:14:54 -0700

    Agregada documentacion del mapper de restaurantes

--- VacanzeApi/Persistence/DAO/Grupo7/IRestaurantDAO.cs
+++ VacanzeApi/Persistence/DAO/Grupo7/IRestaurantDAO.cs
@@ -1,15 +1,48 @@
 using System.Collections.Generic;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo7;
 
-namespace DefaultNamespace
+namespace vacanze_back.VacanzeApi.Persistence.DAO.Grupo7
 {
+    /// <summary>  
+    ///  Interfaz para definir los metodos que debe implementar los DAO de restaurnates
+    /// </summary> 
     public interface IRestaurantDAO
     {
+        
+        /// <summary>
+        ///     Metodo para obtener todos los restaurantes 
+        /// </summary>
+        /// <returns>Restaurant Entity</returns>
         List<Restaurant> GetRestaurants();
+        /// <summary>
+        ///     Metodo para obtener un restaurante segun su id
+        /// </summary>
+        /// <param name="id">id del restaurante a buscar</param>
+        /// <returns>Restaurant Entity</returns>
         Restaurant GetRestaurant(int id);
-        List<Restaurant> GetRestaurantsByCity(int location_id);
+        /// <summary>
+        ///     Metodo para obtener todos los restaurantes de una ciudad dada
+        /// </summary>
+        /// <param name="locationId">Identificador unico de la ciudad a la que pertenecen los restaurantes</param>
+        /// <returns>Lista de restaurantes</returns>
+        List<Restaurant> GetRestaurantsByCity(int locationId);
+        
+        /// <summary>
+        ///     Metodo para agregar un restaurante
+        /// </summary>
+        /// <param name="restaurant">Objeto Restaurant a crear</param>
+        /// <returns>id del restaurante agregado</returns>
         int AddRestaurant(Restaurant restaurant);
+        /// <summary>
+        ///     Metodo para modifcar un restaurant
+        /// </summary>
+        /// <param name="restaurant">Objeto restaurant con la data para el restaurant a modificar</param>
+        /// <returns>Objeto tipo restaurant modificado</returns>
         Restaurant UpdateRestaurant(Restaurant restaurant);
+        /// <summary>
+        ///     Metodo para eliminar un restaurante segun su id
+        /// </summary>
+        /// <param name="id">id del restaurante a eliminar</param>
         void DeleteRestaurant(int id);
     }
 }
--- VacanzeApi/Persistence/DAO/Grupo7/PostgresRestaurantDAO.cs
+++ VacanzeApi/Persistence/DAO/Grupo7/PostgresRestaurantDAO.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using DefaultNamespace;
-using Npgsql;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo7;
 using vacanze_back.VacanzeApi.Common.Exceptions;
 
@@ -10,7 +8,12 @@ namespace vacanze_back.VacanzeApi.Persistence.DAO.Grupo7
 {
     public class PostgresRestaurantDAO: IRestaurantDAO
     {
-        
+        /// <summary>
+        ///     Metodo para obtener un restaurante segun su id
+        /// </summary>
+        /// <param name="id">id del restaurante a buscar</param>
+        /// <returns>Restaurant Entity</returns>
+        /// <exception cref="RestaurantNotFoundExeption">No se encontro ningun restaurante con el id suministrado</exception>
         public Restaurant GetRestaurant(int id)
         {       
 
@@ -20,6 +23,10 @@ namespace vacanze_back.VacanzeApi.Persistence.DAO.Grupo7
                 return ExtractRestaurantFromRow(table.Rows[0]);
         }
         
+        /// <summary>
+        ///     Metodo para obtener todos los restaurantes
+        /// </summary>
+        /// <returns>Lista de restaurantes</returns>
         public List<Restaurant> GetRestaurants()
         {
 
@@ -33,6 +40,11 @@ namespace vacanze_back.VacanzeApi.Persistence.DAO.Grupo7
             return restaurantList;
         }
         
+        /// <summary>
+        ///     Metodo para obtener todos los restaurantes de una ciudad dada
+        /// </summary>
+        /// <param name="locationId">Identificador unico de la ciudad a la que pertenecen los restaurantes</param>
+        /// <returns>Lista de restaurantes</returns>
         public List<Restaurant> GetRestaurantsByCity(int locationId)
         {
             var table = PgConnection.Instance.ExecuteFunction("GetRestaurantsByCity(@location_id)", locationId);
@@ -45,6 +57,11 @@ namespace vacanze_back.VacanzeApi.Persistence.DAO.Grupo7
             return restaurantList;
         }
         
+        /// <summary>
+        ///     Metodo para agregar un restaurante
+        /// </summary>
+        /// <param name="restaurant">Objeto Restaurant a crear</param>
+        /// <returns>id del restaurante agregado</returns>
         public int AddRestaurant(Restaurant restaurant)
         {
             var table = PgConnection.Instance.ExecuteFunction(
@@ -56,6 +73,12 @@ namespace vacanze_back.VacanzeApi.Persistence.DAO.Grupo7
             return savedId;
         }
         
+        /// <summary>
+        ///     Metodo para modifcar un restaurant
+        /// </summary>
+        /// <param name="restaurant">Objeto restaurant con la data para el restaurant a modificar</param>
+        /// <returns>Objeto tipo restaurant modificado</returns>
+        /// <exception cref="RestaurantNotFoundExeption">Retorna BadRequest en caso de no encontrar el restaurante a actualizar</exception>
         public Restaurant UpdateRestaurant(Restaurant restaurant)
         {
             var table = PgConnection.Instance.ExecuteFunction("GetRestaurant(@restaurant_id)" , restaurant.Id);
@@ -71,11 +94,19 @@ namespace vacanze_back.VacanzeApi.Persistence.DAO.Grupo7
             return GetRestaurant(restaurant.Id);
         }
 
+        /// <summary>
+        ///     Metodo para eliminar un restaurante segun su id
+        /// </summary>
+        /// <param name="id">id del restaurante a eliminar</param>
         public void DeleteRestaurant(int id)
         {
             PgConnection.Instance.ExecuteFunction("DeleteRestaurant(@id)",id);
         }
 
+        /// <summary>
+        ///     Metodo extraer un restaurante de la tabla retornada por las consultas a la base de datos ejecutadas por el PgConnection
+        /// </summary>
+        /// <param name="row">fila a extraer</param>
         private Restaurant ExtractRestaurantFromRow(DataRow row)
         {
             var restaurantId = Convert.ToInt32(row[0]);
