4b81c25fd1760117e37c6a76fff3a81b88f452de
Fernando Consalvo <fconsalvo97@gmail.com>
2019-07-07 16:45:37 -0400

    Feat: Se agrego la fabrica de las entidades del modulo

--- VacanzeApi/Common/Entities/EntityFactory.cs
+++ VacanzeApi/Common/Entities/EntityFactory.cs
@@ -1,25 +1,30 @@
+using System;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo13;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo5;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo6;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo7;
 
-namespace vacanze_back.VacanzeApi.Common.Entities{
-
-    public class EntityFactory{
-
-        public static Brand createBrand(string brandName){
+namespace vacanze_back.VacanzeApi.Common.Entities
+{
+    public class EntityFactory
+    {
+        public static Brand createBrand(string brandName)
+        {
             return new Brand(brandName);
         }
 
-        public static Restaurant CreateRestaurant(int id, string name, int capacity,bool isActive,decimal qualify, string specialty, 
-            decimal price, string businessName, string picture, 
+        public static Restaurant CreateRestaurant(int id, string name, int capacity, bool isActive, decimal qualify,
+            string specialty,
+            decimal price, string businessName, string picture,
             string description, string phone, int location, string address)
         {
-            return new Restaurant(id,name,capacity,isActive,qualify,specialty,price,businessName,picture,description,phone,location,address);
+            return new Restaurant(id, name, capacity, isActive, qualify, specialty, price, businessName, picture,
+                description, phone, location, address);
         }
 
-        public static Hotel createHotel(int id , string name , int amountOfRooms, int roomCapacity ,
-            bool isActive, string addressSpecs, decimal pricePerRoom, string website , string phone ,
-            string picture, int stars , int locationId )
+        public static Hotel createHotel(int id, string name, int amountOfRooms, int roomCapacity,
+            bool isActive, string addressSpecs, decimal pricePerRoom, string website, string phone,
+            string picture, int stars, int locationId)
         {
             return HotelBuilder.Create()
                 .IdentifiedBy(id)
@@ -31,14 +36,27 @@ namespace vacanze_back.VacanzeApi.Common.Entities{
                 .WithWebsite(website)
                 .WithStars(stars)
                 .WithPictureUrl(picture)
-                .LocatedAt(locationId) 
+                .LocatedAt(locationId)
                 .WithStatus(isActive)
                 .WithAddressDescription(addressSpecs)
                 .BuildSinVaidar();
         }
 
-        public static Location CreateLocation(int id, string country, string city){
+        public static Location CreateLocation(int id, string country, string city)
+        {
             return new Location(id, country, city);
         }
+
+        #region Grupo 13
+        public static ReservationAutomobile CreateReservationAutomobile(int id, DateTime checkIn, DateTime checkOut) =>
+            new ReservationAutomobile(id, checkIn, checkOut);
+
+
+        public static ReservationRoom CreateReservationRoom(int id, DateTime chekckIn, DateTime checkOut) =>
+            new ReservationRoom(id, chekckIn, checkOut);
+        
+        public static ReservationRoom CreateReservationRoom() =>
+            new ReservationRoom();
+        #endregion
     }
 }
