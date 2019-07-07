b771c7894f3d4c5a9ccc715ba5305fa3c468d3d3
Jhonni Calle <jhonnicalle@gmail.com>
2019-07-06 21:22:37 -0400

    Agregando archivos al DAO Grupo 14

---
+++ VacanzeApi/Persistence/DAO/Grupo14/ReservationRestaurantDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vacanze_back.VacanzeApi.Common.Entities;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo14;
+
+
+namespace vacanze_back.VacanzeApi.Persistence.DAO.Grupo14
+{
+    public interface ReservationRestaurantDAO
+    {
+        int addReservation(Restaurant_res reserva);
+        List<Restaurant_res> getResRestaurant(int user);
+        List<Restaurant_res> getReservationNotPay(int user);
+        int deleteResRestaurant(int resRestId);
+        int updateResRestaurant(int payID, int resRestID);
+        
+    }
+}
