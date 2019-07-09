838357b345fbc78c99b0b88cad2d00077bedf953
gabrielbaron16 <gabrielbu161@gmail.com>
2019-07-09 19:35:08 -0400

    Añadiendo Atributo Id para DTO

--- VacanzeApi/LogicLayer/DTO/Grupo13/ReservationRoomDTO.cs
+++ VacanzeApi/LogicLayer/DTO/Grupo13/ReservationRoomDTO.cs
@@ -1,18 +1,21 @@
 using System;
+using Org.BouncyCastle.Crypto.Engines;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo2;
 
 namespace vacanze_back.VacanzeApi.LogicLayer.DTO.Grupo13
 {
     public class ReservationRoomDTO
     {
+        public int Id { get; set; }
+        
         public DateTime CheckIn { get; set; }
         
         public DateTime CheckOut { get; set; }
         
-        public int Hotel { get; set; }
+        public int HotelId { get; set; }
 
-        public int Fk_user { get; set; }
+        public int UserId { get; set; }
 
-        public int Fk_pay{ get; set; }
+        public int PayId { get; set; }
     }
 }
