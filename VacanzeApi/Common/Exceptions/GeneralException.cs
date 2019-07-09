1bd58d1846fd5c97a1688d5b22740d1a7df57eb3
Ignacio Paradisi <ignacioparadisi@gmail.com>
2019-07-09 19:12:41 -0400

    Se arreglo un error de GeneralException

--- VacanzeApi/Common/Exceptions/GeneralException.cs
+++ VacanzeApi/Common/Exceptions/GeneralException.cs
@@ -4,6 +4,9 @@ namespace vacanze_back.VacanzeApi.Common.Exceptions
 {
     public class GeneralException :Exception
     {
+        public GeneralException()
+        {
+        }
         public GeneralException(string message) : base(message)
         {
         }
