237d6035f64f280403c52e366e9edf0e3beceed3
cloudyrl <cloudyrl1@gmail.com>
2019-07-10 14:58:38 -0700

    Pruebas de notfound agregadas al controlador de restaurantes

--- VacanzeApiTest/Grupo7/RestaurantControllerTest.cs
+++ VacanzeApiTest/Grupo7/RestaurantControllerTest.cs
@@ -80,7 +80,7 @@ namespace vacanze_back.VacanzeApiTest.Grupo7
 
         public void GetRestaurantTest_RestaurantNotFoundExeption_ReturnBadRequest()
         {
-            Assert.IsInstanceOf<BadRequestObjectResult>(_restaurantsController.GetRestaurant(-1).Result);
+            Assert.IsInstanceOf<NotFoundObjectResult>(_restaurantsController.GetRestaurant(-1).Result);
         }
 
         [Test]
@@ -108,7 +108,7 @@ namespace vacanze_back.VacanzeApiTest.Grupo7
         public void PutRestaurantTest_RestaurantNotFoundExeption_ReturnBadRequest()
         {
             _restaurantDto.Id = -1;
-            Assert.IsInstanceOf<BadRequestObjectResult>(_restaurantsController.PutRestaurant(_restaurantDto).Result);
+            Assert.IsInstanceOf<NotFoundObjectResult>(_restaurantsController.PutRestaurant(_restaurantDto).Result);
         }
         
         [Test]
