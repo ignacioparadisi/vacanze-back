286974e90faa66606e59e63b29492af182a435be
Juan Goncalves <juan.goncalves498@gmail.com>
2019-07-10 09:14:03 -0400

    test: agregados mas casos de prueba para el PUT de Claim

--- VacanzeApiTest/Grupo9/ClaimControllerTests.cs
+++ VacanzeApiTest/Grupo9/ClaimControllerTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo9;
+using vacanze_back.VacanzeApi.Common.Exceptions;
 using vacanze_back.VacanzeApi.LogicLayer.DTO.Grupo9;
 using vacanze_back.VacanzeApi.LogicLayer.Mapper;
 using vacanze_back.VacanzeApi.LogicLayer.Mapper.Grupo9;
@@ -127,15 +128,79 @@ namespace vacanze_back.VacanzeApiTest.Grupo9
 
 
         [Test]
-        public void Put_Claim_OkResultReturned()
+        public void Put_UpdatedTitleAndDescription_OkResultReturned()
         {
             var savedClaimId = _postgresClaimDao.Add(_claimMapper.CreateEntity(_claimDto));
             _insertedClaims.Add(savedClaimId);
-            _claimDto.Description = "updated";
-            var result = _claimController.Put(savedClaimId,_claimDto);
-            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var fieldsToUpdate = new ClaimDto()
+            {
+                Title = "New title",
+                Description = "New description"
+            };
+            var result = _claimController.Put(savedClaimId,fieldsToUpdate);
+            Assert.IsInstanceOf<OkResult>(result.Result);
         }
 
+        [Test]
+        public void Put_UpdatedTitleAndDescription_FieldsActuallyUpdated()
+        {
+            var savedClaimId = _postgresClaimDao.Add(_claimMapper.CreateEntity(_claimDto));
+            _insertedClaims.Add(savedClaimId);
+            var fieldsToUpdate = new ClaimDto()
+            {
+                Title = "New title",
+                Description = "New description"
+            };
+            _claimController.Put(savedClaimId,fieldsToUpdate);
+            var claimInDb = _postgresClaimDao.GetById(savedClaimId);
+            Assert.AreEqual(fieldsToUpdate.Title, claimInDb.Title);
+            Assert.AreEqual(fieldsToUpdate.Description, claimInDb.Description);
+        }
         
+        [Test]
+        public void Put_InvalidClaim_BadRequestReturned()
+        {
+            var fieldsToUpdate = new ClaimDto()
+            {
+                Title = "New title",
+                Description = "New description"
+            };
+            var result = _claimController.Put(-1, fieldsToUpdate);
+            Assert.IsInstanceOf<BadRequestObjectResult>(result.Result);
+        }
+        
+        [Test]
+        public void Put_InvalidClaimTitle_BadRequestReturned()
+        {
+            var savedClaimId = _postgresClaimDao.Add(_claimMapper.CreateEntity(_claimDto));
+            _insertedClaims.Add(savedClaimId);
+            var fieldsToUpdate = new ClaimDto()
+            {
+                Title = "SUPER LONG TITLE ............................................................................",
+                Description = "New description"
+            };
+            var result = _claimController.Put(savedClaimId, fieldsToUpdate);
+            Assert.IsInstanceOf<BadRequestObjectResult>(result.Result);
+        }
+        
+        [Test]
+        public void Put_InvalidClaimStatus_BadRequestReturned()
+        {
+            var fieldsToUpdate = new ClaimDto()
+            {
+                Status = "NOTVALIDSTATUS"
+            };
+            var result = _claimController.Put(-1, fieldsToUpdate);
+            Assert.IsInstanceOf<BadRequestObjectResult>(result.Result);
+        }
+        
+        [Test]
+        public void Delete_ExistingClaim_ClaimIsDeleted()
+        {
+            var savedClaimId = _postgresClaimDao.Add(_claimMapper.CreateEntity(_claimDto));
+            _insertedClaims.Add(savedClaimId);
+            _claimController.Delete(savedClaimId);
+            Assert.Throws<ClaimNotFoundException>(() => { _postgresClaimDao.GetById(savedClaimId); });
+        }
     }
 }
