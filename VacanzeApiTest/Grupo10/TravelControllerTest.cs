6d080b6642c4e2feec79b91473e23f74de076712
Yisusrly <45052243+Yisusrly@users.noreply.github.com>
2019-07-10 12:36:52 -0400

    preparando para hacer PU

--- VacanzeApiTest/Grupo10/TravelControllerTest.cs
+++ VacanzeApiTest/Grupo10/TravelControllerTest.cs
@@ -6,15 +6,28 @@ using vacanze_back.VacanzeApi.Persistence.Repository;
 using vacanze_back.VacanzeApi.Persistence.Repository.Grupo10;
 using vacanze_back.VacanzeApi.Common.Exceptions;
 using vacanze_back.VacanzeApi.Common.Exceptions.Grupo10;
+using vacanze_back.VacanzeApi.Common.Exceptions.Grupo12;
+using vacanze_back.VacanzeApi.Common.Exceptions.Grupo13;
+using vacanze_back.VacanzeApi.Common.Exceptions.Grupo14;
 using vacanze_back.VacanzeApi.Common.Entities;
 using vacanze_back.VacanzeApi.Common.Entities.Grupo10;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo12;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo13;
+using vacanze_back.VacanzeApi.Common.Entities.Grupo14;
+using vacanze_back.VacanzeApi.Services.Controllers;
 using vacanze_back.VacanzeApi.Services.Controllers.Grupo10;
+using vacanze_back.VacanzeApi.Services.Controllers.Grupo12;
+using vacanze_back.VacanzeApi.Services.Controllers.Grupo13;
+using vacanze_back.VacanzeApi.Services.Controllers.Grupo14;
+
 
 namespace vacanze_back.VacanzeApiTest.Grupo10
 {   
     [TestFixture]
-    public class TravelControllerTest{
-       
-
+    public class TravelControllerTest
+    {
+        private int addId;
+        private int modifyId;
+        private int deleteId;
     }
 }
