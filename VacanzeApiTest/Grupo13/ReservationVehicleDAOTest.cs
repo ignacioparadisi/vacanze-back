416eda7c7b27771131c889d17761c1d6f2ce43a9
Fernando Consalvo <fconsalvo97@gmail.com>
2019-07-09 23:35:39 -0400

    Feat: Se agregaron las pruebas de los métodos restantes del DAO

--- VacanzeApiTest/Grupo13/ReservationVehicleDAOTest.cs
+++ VacanzeApiTest/Grupo13/ReservationVehicleDAOTest.cs
@@ -43,7 +43,7 @@ namespace vacanze_back.VacanzeApiTest.Grupo13
                 .GetReservationVehicleDAO();
             DateTime checkOut = new DateTime(2019, 10, 20);
             _reservationAutomobile =
-                EntityFactory.CreateReservationAutomobile(0, DateTime.Now, checkOut, _user.Id, 5);
+                EntityFactory.CreateReservationVehicle(0, DateTime.Now, checkOut, _user.Id, 5);
             _reservationAutomobile = dao.AddReservation(_reservationAutomobile);
             Assert.NotZero(_reservationAutomobile.Id);
         }
